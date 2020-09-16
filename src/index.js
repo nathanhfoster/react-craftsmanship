34027263b88cf39c8b0847b2a83012aed55543b9
Nathan Foster <nathan.foster@passportinc.com>
2020-09-16 08:11:21 -0700

    Fix

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from "react-dom"
 import "index.css"
 import App from "App"
 import storeFactory from "./redux"
-import { reducers } from "./redux/reducers"
 import { Provider } from "react-redux"
-import { ContextProvider } from "store"
 import * as serviceWorker from "./serviceWorker"
 
 const store = storeFactory()
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <ContextProvider reducers={reducers}> */}
     <App />
-    {/* </ContextProvider>, */}
   </Provider>,
   document.getElementById("root")
 )
