f9e6437dfcb19c5da17d201b85a7511bd97ecb42
Nathan Foster <nathan.foster@passportinc.com>
2021-03-29 13:53:56 -0700

    areMergedPropsEqual

--- src/context/index.js
+++ src/context/index.js
@@ -2,10 +2,10 @@ import { createContext } from 'react'
 
 const FormWithUseContextAndReducerContext = createContext(null)
 
-// we implement areStatePropsEqual to only mount the input fields once to the DOM
+// we implement areMergedPropsEqual to only mount the input fields once to the DOM
 const FormWithUseContextAndReducerOptions = {
   context: FormWithUseContextAndReducerContext,
-  areStatePropsEqual: (prevState, nextState) => {
+  areMergedPropsEqual: (prevState, nextState) => {
     const areEqual = prevState.renderInputs.length === nextState.renderInputs.length
 
     return areEqual
@@ -14,7 +14,7 @@ const FormWithUseContextAndReducerOptions = {
 
 const FormInputWithUseContextAndReducerOptions = {
   context: FormWithUseContextAndReducerContext,
-  areStatePropsEqual: (prevState, nextState) => {
+  areMergedPropsEqual: (prevState, nextState) => {
     const areEqual = prevState.value === nextState.value
 
     return areEqual
