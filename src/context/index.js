60d53dcaa563c1c34e244a5b35ee187a6a24bc99
Nate <nathan@ecocart.io>
2021-05-26 11:24:39 -0700

    Update

--- src/context/index.js
+++ src/context/index.js
@@ -1,28 +1,34 @@
-import { createContext } from 'react'
+import React, { createContext } from 'react';
 
-const FormWithUseContextAndReducerContext = createContext(null)
+const FormWithUseContextAndReducerContext = createContext(null);
 
 // we implement areMergedPropsEqual to only mount the input fields once to the DOM
 const FormWithUseContextAndReducerOptions = {
   context: FormWithUseContextAndReducerContext,
   areMergedPropsEqual: (prevState, nextState) => {
-    const areEqual = prevState.renderInputs.length === nextState.renderInputs.length
-
-    return areEqual
+    const areEqual = prevState.renderInputs.length === nextState.renderInputs.length;
+    console.log(
+      FormWithUseContextAndReducerContext,
+      FormWithUseContextAndReducerContext.__proto__.isPrototypeOf(
+        FormWithUseContextAndReducerContext,
+      ),
+      FormWithUseContextAndReducerContext.$$typeof == 'Symbol(react.context)',
+    );
+    return areEqual;
   },
-}
+};
 
 const FormInputWithUseContextAndReducerOptions = {
   context: FormWithUseContextAndReducerContext,
   areMergedPropsEqual: (prevState, nextState) => {
-    const areEqual = prevState.value === nextState.value && prevState.type === nextState.type
+    const areEqual = prevState.value === nextState.value && prevState.type === nextState.type;
 
-    return areEqual
+    return areEqual;
   },
-}
+};
 
 export {
   FormWithUseContextAndReducerContext,
   FormWithUseContextAndReducerOptions,
   FormInputWithUseContextAndReducerOptions,
-}
+};
